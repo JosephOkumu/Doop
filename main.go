1652a67e56814f977d6a97b453172b79b9da0cba
JosephOkumu <jokumu25@gmail.com>
2024-06-14 12:55:36 +0300

    Changing the validate function to return boolean value

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	
 	"os"
 )
 
@@ -14,23 +13,22 @@ func atoi(str string) (int, bool) {
 			sign = 1
 		} else if i == 0 && v == '-' {
 			sign = -1
-		} else if v >= 48 && v <= 57 {
-			num = num*10 + int(v-48)
+		} else if v >= '0' && v <= '9' {
+			num = num*10 + int(v-'0')
 		} else {
 			return 0, true
 		}
 	}
-	result:=sign*num
+	result := sign * num
 
 	// handling overflows in atoi
 	if str[0] == '-' && result > 0 {
 		return 0, true // Overflow detected
 	} else if str[0] != '-' && result < 0 {
 		return 0, true // overflow detected
-	} 
-	
-	return result, false // no overflow
+	}
 
+	return result, false // no overflow
 }
 
 func add(a, b int) int {
@@ -54,7 +52,6 @@ func subtract(a, b int) int {
 }
 
 func itoa(num int) string {
-
 	if num == 0 {
 		return "0"
 	}
@@ -66,7 +63,7 @@ func itoa(num int) string {
 	slais := []byte{}
 	for num > 0 {
 		digit := num % 10
-		slais = append(slais, byte(digit)+48)
+		slais = append(slais, byte(digit)+'0')
 		num /= 10
 	}
 
@@ -78,19 +75,25 @@ func itoa(num int) string {
 	return sign + string(slais)
 }
 
-func validate(str1 string) {
-	for i, v := range str1 {
-		if i == 0 && v == '-' || v == '+'{
+func validate(str string) bool {
+	if str == "" {
+		return false
+	}
+
+	for i, v := range str {
+		if i == 0 && (v == '-' || v == '+') {
 			continue
 		}
-		if v < 48 || v > 57 {
-			return
+		if v < '0' || v > '9' {
+			return false
 		}
 	}
+	return true
 }
 
 func main() {
 	if len(os.Args) != 4 {
+		os.Stdout.WriteString("Usage: program <value1> <operator> <value2>\n")
 		return
 	}
 
@@ -99,55 +102,64 @@ func main() {
 	value3 := os.Args[3]
 
 	// handling invalid values
-	validate(value1)
-	validate(value3)
-
-	// handling invalid operators
-	if ops != "+" && ops != "-" && ops != "/" && ops != "%" && ops != "*" {
+	if !validate(value1) || !validate(value3) {
+		os.Stdout.WriteString("Invalid number input\n")
 		return
 	}
 
-	//Converting value1 and value2 to integer
+	// handling invalid operators
+	if ops != "+" && ops != "-" && ops != "/" && ops != "%" && ops != "*" {
+		os.Stdout.WriteString("Invalid operator\n")
+		return
+	}
+
+	// Converting value1 and value3 to integer
 	num1, overflow := atoi(value1)
 	if overflow {
+		os.Stdout.WriteString("Number overflow\n")
 		return
 	}
 	num2, overflow := atoi(value3)
 	if overflow {
+		os.Stdout.WriteString("Number overflow\n")
 		return
 	}
 
 	// implementing operations
 	var result int
-	if ops == "+" {
+	switch ops {
+	case "+":
 		if num1 > 0 && num2 > 0 && add(num1, num2) < 0 {
+			os.Stdout.WriteString("Addition overflow\n")
 			return
 		}
 		result = add(num1, num2)
-	} else if ops == "-" {
+	case "-":
 		if num1 < 0 && num2 < 0 && subtract(num1, num2) > 0 {
+			os.Stdout.WriteString("Subtraction overflow\n")
 			return
-		} 
+		}
 		result = subtract(num1, num2)
-	} else if ops == "/" {
-		if num2 == 0{
+	case "/":
+		if num2 == 0 {
 			os.Stdout.WriteString("No division by 0\n")
 			return
-		}		
+		}
 		result = divide(num1, num2)
-	} else if ops == "%" {
-		if num2 == 0{
+	case "%":
+		if num2 == 0 {
 			os.Stdout.WriteString("No modulo by 0\n")
 			return
 		}
 		result = modulo(num1, num2)
-	} else if ops == "*" {
-		if num1 != 0 && multiply(num1, num2) / num1 != num2 {
+	case "*":
+		if num1 != 0 && multiply(num1, num2)/num1 != num2 {
+			os.Stdout.WriteString("Multiplication overflow\n")
 			return
 		}
 		result = multiply(num1, num2)
 	}
+
 	finalResult := itoa(result)
-	// fmt.Println(finalResult)
 	os.Stdout.WriteString(finalResult + "\n")
 }
